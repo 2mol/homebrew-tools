117471888e39633abe059ab0d32ba046783121fd
2mol <2mol@users.noreply.github.com>
2018-07-03 17:58:48 +0200

    brew audit fixes

--- pboy.rb
+++ pboy.rb
@@ -1,12 +1,12 @@
 class Pboy < Formula
-  desc "a small .pdf management utility"
+  desc "Small .pdf management utility"
   homepage "https://github.com/2mol/pboy"
   url "https://github.com/2mol/pboy/releases/download/0.1.0/pboy-0.1.0-osx.tar.gz"
   sha256 "cc7959f002ceed3c954a5bdcf32af4f86c3b33d7b8ca672b0ad4e44eaba5c8be"
 
-  depends_on "poppler"
-
   bottle :unneeded
+
+  depends_on "poppler"
 
   def install
     bin.install "pboy"
