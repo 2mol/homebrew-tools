08bc65689b37da768951b44b333f3ea7dbee448d
2mol <2mol@users.noreply.github.com>
2019-04-21 15:15:35 +0200

    fixed pboy download link

--- pboy.rb
+++ pboy.rb
@@ -1,7 +1,7 @@
 class Pboy < Formula
   desc "Small .pdf management utility"
   homepage "https://github.com/2mol/pboy"
-  url "https://github.com/2mol/pboy/releases/download/1.1/pboy-1.2-osx.tar.gz"
+  url "https://github.com/2mol/pboy/releases/download/1.2/pboy-1.2-osx.tar.gz"
   sha256 "444984d2228963cef6151559291c44d8c2603787f8e7f352e91d738fd37bca84"
 
   bottle :unneeded
